3e298f6cb2af6a7f024c19eb1ca1772d32fc868e
Xu <837456906@qq.com>
2018-03-01 14:51:54 +0800

    functions of regist,log in,forget are finished except the internet accessing

--- YueLi/app/src/main/java/com/example/jeff/yueli/ForgetActivity.java
+++ YueLi/app/src/main/java/com/example/jeff/yueli/ForgetActivity.java
@@ -1,7 +1,12 @@
 package com.example.jeff.yueli;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ForgetActivity extends AppCompatActivity {
 
@@ -9,5 +14,41 @@ public class ForgetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget);
+        Button forget = (Button)findViewById(R.id.log_button);
+        Button back = (Button)findViewById(R.id.back_to_login);
+        //绑定忘记密码和返回事件
+        forget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText userName = (EditText)findViewById(R.id.name_input);
+                EditText passWord = (EditText)findViewById(R.id.psw_input);
+                String username = userName.getText().toString();
+                String password = passWord.getText().toString();
+                if (isUserExist(username)) {
+                    sendChangedUser(username, password);
+                    Toast.makeText(ForgetActivity.this, "密码更改成功", Toast.LENGTH_SHORT);
+                    // 跳转主页面->!
+                } else {
+                    Toast.makeText(ForgetActivity.this, "用户名不存在", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ForgetActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    // 判断用户名是否被注册
+    boolean isUserExist(String userName) {
+        return true;
+    }
+
+    // 提交更改
+    void sendChangedUser(String userName, String pwd) {
+
     }
 }
--- YueLi/app/src/main/java/com/example/jeff/yueli/LoginActivity.java
+++ YueLi/app/src/main/java/com/example/jeff/yueli/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.example.jeff.yueli;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,57 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button login = (Button)findViewById(R.id.log_button);
+        Button forget = (Button)findViewById(R.id.forget_button);
+        Button regist = (Button)findViewById(R.id.register_button);
+        // 注册、登录、忘记密码按钮绑定
+        regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        forget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgetActivity.class);
+                startActivity(intent);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText userName = (EditText)findViewById(R.id.name_input);
+                EditText passWord = (EditText)findViewById(R.id.psw_input);
+                String username = userName.getText().toString();
+                String password = passWord.getText().toString();
+                if (isUserExist(username)) {
+                    if (password.equals(getPassWord(username))) {
+                        // 跳转主页面->!
+                    } else {
+                        Toast.makeText(LoginActivity.this, "密码错误", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "用户名不存在", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+
+    // 网络访问模块
+
+    // 获取用户名对应的密码
+    String getPassWord(String usesrName) {
+        String res = "";
+        return res;
+    }
+
+    // 判断用户是否存在
+    boolean isUserExist(String userName) {
+
+        return true;
     }
 }
