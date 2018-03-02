194348a1eba3981f857cb8661d419a9e03ca9f6e
Xu Muxin <xumuxin@XudeMacBook-Pro.local>
2018-03-02 11:44:31 +0800

    Login&&Forget&&Register_Activity

--- YueLi/app/src/main/java/com/example/jeff/yueli/LoginActivity.java
+++ YueLi/app/src/main/java/com/example/jeff/yueli/LoginActivity.java
@@ -9,6 +9,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.CookieStore;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import com.example.jeff.yueli.signin;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -38,38 +60,69 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText userName = (EditText)findViewById(R.id.name_input);
                 EditText passWord = (EditText)findViewById(R.id.psw_input);
-                String username = userName.getText().toString();
-                String password = passWord.getText().toString();
-                //for test
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-
-                if (isUserExist(username)) {
-                    if (password.equals(getPassWord(username))) {
-                        // 跳转主页面->!
-                    } else {
-                        Toast.makeText(LoginActivity.this, "密码错误", Toast.LENGTH_SHORT).show();
+                final String username = userName.getText().toString();
+                final String password = passWord.getText().toString();
+                String url="http://private-aa8865-yueliapi.apiary-mock.com/api/users/login?username="+username+"&password="+password;
+                OkHttpClient httpClient = new OkHttpClient.Builder()
+                        .cookieJar(new CookieJar() {
+                            private final java.util.Map<String, List<Cookie>> cookiesMap = new HashMap<String, List<Cookie>>();
+                            @Override
+                            public void saveFromResponse(HttpUrl arg0, List<Cookie> arg1) {
+                                // TODO Auto-generated method stub
+                                //移除相同的url的Cookie
+                                String host = arg0.host();
+                                List<Cookie> cookiesList = cookiesMap.get(host);
+                                if (cookiesList != null){
+                                    cookiesMap.remove(host);
+                                }
+                                //再重新天添加
+                                cookiesMap.put(host, arg1);
+                            }
+                            @Override
+                            public List<Cookie> loadForRequest(HttpUrl arg0) {
+                                // TODO Auto-generated method stub
+                                List<Cookie> cookiesList = cookiesMap.get(arg0.host());
+                                //注：这里不能返回null，否则会报NULLException的错误。
+                                //原因：当Request 连接到网络的时候，OkHttp会调用loadForRequest()
+                                return cookiesList != null ? cookiesList : new ArrayList<Cookie>();
+                            }
+                        })
+                        .addInterceptor(new HttpLoggingInterceptor()).build();
+                Request request = new Request.Builder().url(url).build();
+                httpClient.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
                     }
-                } else {
-                    Toast.makeText(LoginActivity.this, "用户名不存在", Toast.LENGTH_SHORT).show();
-                }
+                    String string=null;
+                    @Override
+                    public void onResponse(Call call, final Response response) throws IOException {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    string = response.body().string();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                                Gson gson = new Gson();
+                                Type logintype = new TypeToken<Result<signin>>(){}.getType();
+                                Result<signin> loginresult = gson.fromJson(string, logintype);
+                                signin denglu = loginresult.data;
+
+                                int rescode = response.code();
+                                if (rescode == 200) {
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(LoginActivity.this, loginresult.msg, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                });
 
             }
         });
     }
 
-
-    // 网络访问模块
-
-    // 获取用户名对应的密码
-    String getPassWord(String usesrName) {
-        String res = "";
-        return res;
-    }
-
-    // 判断用户是否存在
-    boolean isUserExist(String userName) {
-
-        return true;
-    }
 }
