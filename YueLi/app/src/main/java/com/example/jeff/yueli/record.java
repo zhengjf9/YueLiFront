6a8591822fa8442dd1fbcee1b40217bfa1b3d8e5
Mu-xsan <904984318@qq.com>
2018-03-04 13:26:05 +0800

    for record data

--- YueLi/app/src/main/java/com/example/jeff/yueli/record.java
+++ YueLi/app/src/main/java/com/example/jeff/yueli/record.java
@@ -1,28 +1,55 @@
 package com.example.jeff.yueli;
 
+import java.util.List;
+
 /**
  * Created by xumuxin on 2018/3/3.
  */
 
 public class record {
-    private int travel_record_id;
-    private int spot_id;
-    private String spot_name;
-    private String content;
-    private String time;
+    private List<Rec> data;
+    private String msg;
 
-    public int gettravelrecordid() {return travel_record_id;}
-    public int getspotid() {
-        return spot_id;
+    public void setmsg(String msg) {
+        this.msg = msg;
     }
-    public String getspotname() {return spot_name;}
-    public String getcontent() {return content;}
-    public String gettime() {return time;}
 
-    public void settravelrecordid(int id) {travel_record_id = id;}
-    public void setspotid(int id) {spot_id = id;}
+    public void setdata(List<Rec> recordinfo) {
+        this.data = recordinfo;
+    }
 
-    public void setspotname(String name) {spot_name = name;}
-    public void setContent(String t) {content = t;}
-    public void settime(String t) {time = t;}
+    public String getmsg() {
+        return msg;
+    }
+
+    public List<Rec> gettrips() {
+        return data;
+    }
+
+    public static class Rec {
+        private int travel_record_id;
+        private int spot_id;
+        private String spot_name;
+        private String content;
+        private String time;
+        private int day;
+
+        public int gettravelrecordid() {return travel_record_id;}
+        public int getspotid() {
+            return spot_id;
+        }
+        public String getspotname() {return spot_name;}
+        public String getcontent() {return content;}
+        public String gettime() {return time;}
+        public int getday() {return day;}
+
+        public void settravelrecordid(int id) {travel_record_id = id;}
+        public void setspotid(int id) {spot_id = id;}
+
+        public void setspotname(String name) {spot_name = name;}
+        public void setContent(String t) {content = t;}
+        public void settime(String t) {time = t;}
+        public void setday(int d) {day=d;}
+    }
+
 }
