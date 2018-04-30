d4a7be243e4cb2a244c221d258c7a1d154235084
jeff <jeff@Ubuntu16.04>
2018-04-30 17:54:55 +0800

    some fix

--- YueLi/app/src/main/java/com/example/jeff/yueli/WelcomeGuideActivity.java
+++ YueLi/app/src/main/java/com/example/jeff/yueli/WelcomeGuideActivity.java
@@ -12,6 +12,8 @@ import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -48,6 +50,9 @@ public class WelcomeGuideActivity extends Activity implements OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_guide);
 
         views = new ArrayList<View>();
